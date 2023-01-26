4864df46ba8868422b0d8cf03a31544d16a84278
Jobayer Hossain <jobayerhossain977@Gmail.com>
2023-01-26 11:23:43 +0600

    staging: Added Change Android Splash Screen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
+import React, { useEffect } from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-import React from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      // SplashScreen?.hide();
+    }, 1500);
+  }, []);
   return (
     <SafeAreaView>
       <Text>Hello World</Text>
--- android/app/src/main/java/com/servicely/MainActivity.java
+++ android/app/src/main/java/com/servicely/MainActivity.java
@@ -1,11 +1,18 @@
 package com.servicely;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import org.devio.rn.splashscreen.SplashScreen; // here
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule

40cdd88e07cbf48ebe6a1473b260b0d888001c54
Jobayer Hossain <jobayerhossain977@Gmail.com>
2023-01-29 18:36:39 +0600

    feat(CRA): :fire: Initial Commit
    Changes App icon for both android and ios

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});

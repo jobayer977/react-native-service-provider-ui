f69991c339ebdba3aa76fd953d6bad1a8eb61c20
Jobayer Hossain <jobayerhossain977@Gmail.com>
2023-01-30 12:08:31 +0600

    day-1: Added android splash

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
+import React, { useEffect } from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-import React from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+    return () => { };
+  }, []);
+
   return (
     <SafeAreaView>
       <View>

f95ca8a843603d8d1933302b4bb57b788b2b23ac
Jobayer Hossain <jobayerhossain977@Gmail.com>
2023-01-30 21:19:12 +0600

    feat(Pre): :zap: Setup Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
+import { Provider } from 'react-redux';
 import SplashScreen from 'react-native-splash-screen';
+import rootReducer from './redux';
 
 const App = () => {
   useEffect(() => {
@@ -10,11 +12,13 @@ const App = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello</Text>
-      </View>
-    </SafeAreaView>
+    <Provider store={rootReducer}>
+      <SafeAreaView>
+        <View>
+          <Text>Hello</Text>
+        </View>
+      </SafeAreaView>
+    </Provider>
   );
 };
 
